3ff256aa7dad668a20c814f149e100581b8f0ec6
proga <fbgamer8374@gmail.com>
2026-03-05 18:50:38 -0500

    made it prettier

--- src/tfg_kpm/commands/manager.py
+++ src/tfg_kpm/commands/manager.py
@@ -6,46 +6,41 @@ import sys
 import io
 import zipfile
 import shutil
-import rich
-from rich.progress import Progress, SpinnerColumn, TextColumn
+from rich.console import Console
 
 
 def install_package(repository: str, branch: str):
+    
+    console = Console()
+    spinner = console.status("Getting package metadata from github")
     if repository.count("/") != 1:
+            console.log("Failed to get package metadata", style="bold red")
             error("Invalid package format, expected [red]author/repo[/red]")
     data = Package.from_git(repository, branch)
-    with Progress(
-        SpinnerColumn(finished_text="✅"),
-        TextColumn("[progress.description]{task.description}"),
-        transient=False,
-    ) as progress:
-        task = progress.add_task(description="Fetching package from github")
-        author, repository = repository.split("/")
-        zip_url = f"https://github.com/{author}/{repository}/archive/refs/heads/{branch}.zip"
-        destination = Path.cwd() / "kubejs" / "server_scripts" / "external_packages" / data.name
+    author, repository = repository.split("/")
+    zip_url = f"https://github.com/{author}/{repository}/archive/refs/heads/{branch}.zip"
+    destination = Path.cwd() / "kubejs" / "server_scripts" / "external_packages" / data.name
     
-        name = package_name(repository, branch)
+    name = package_name(repository, branch)
+    console.log("Finished getting package metadata", style="bold green")
+    if destination.is_dir():
+        error(f"Package [red]{data.name}[/red] is already installed")
+    spinner.update(status="Fetching package archive")
+    destination.mkdir(parents=True, exist_ok=False)
     
-        if destination.is_dir():
-            progress.remove_task(task)
-            error(f"Package [red]{data.name}[/red] is already installed")
+    response = requests.get(zip_url)
     
-        destination.mkdir(parents=True, exist_ok=False)
-    
-        response = requests.get(zip_url)
-    
-        if not response.ok:
-            progress.remove_task(task)
-            error(f"Failed to fetch [red]{name}[/red]")
-        progress.remove_task(task)
-        progress.add_task(description=f"Extracting package files from {name}")
-        with zipfile.ZipFile(io.BytesIO(response.content)) as z:
-            base_folder = f"{repository}-{branch}/"
-            for member in z.namelist():
-                if member.startswith(base_folder + "server_scripts/") or member == base_folder + "registry.toml":
-                    relative_path = member[len(base_folder):]
-                    if relative_path.startswith("server_scripts/"):
-                        relative_path = relative_path[len("server_scripts/"):]
+    if not response.ok:
+        error(f"Failed to fetch [red]{name}[/red]")    
+    console.log("Finished getting package metadata", style="bold green")
+    spinner.update(status="Extracting package archive")
+    with zipfile.ZipFile(io.BytesIO(response.content)) as z:
+        base_folder = f"{repository}-{branch}/"
+        for member in z.namelist():
+            if member.startswith(base_folder + "server_scripts/") or member == base_folder + "registry.toml":
+                relative_path = member[len(base_folder):]
+                if relative_path.startswith("server_scripts/"):
+                    relative_path = relative_path[len("server_scripts/"):]
 
                     target_path = Path(destination) / relative_path
 
@@ -57,43 +52,46 @@ def install_package(repository: str, branch: str):
 
                     with z.open(member) as src, open(target_path, "wb") as dst:
                         shutil.copyfileobj(src, dst)
+    console.log("Finished extracting package to server scripts", style="bold green")
+    spinner.update("Reading main_server_script.js")
     
     main_server_script = Path.cwd() / "kubejs" / "server_scripts" / "main_server_script.js"
     server_lines = []
-    
-    with rich.progress.open(main_server_script, "r", encoding="utf-8", description="reading main_server_script.js") as f:
+        
+    with open(main_server_script, "r", encoding="utf-8") as f:
         server_lines = f.read().splitlines()
-    
+    console.log("Finished reading main_server_script.js", style="bold green")
     recipe_marker = "ServerEvents.recipes(event => {"
     itemtag_marker = "ServerEvents.tags('item', event => {"
     blocktag_marker = "ServerEvents.tags('block', event => {"
     fluidtag_marker = "ServerEvents.tags('fluid', event => {"
-    with Progress(
-        SpinnerColumn(finished_text="✅"),
-        TextColumn("[progress.description]{task.description}"),
-        transient=False,
-    ) as progress:
-        recipetask = progress.add_task(description="Defining recipes in main_server_script.js")
-        for v in data.recipes:
-            insert_after(server_lines, recipe_marker, v)
-            progress.remove_task(recipetask)
-            itemtask = progress.add_task(description="Defining item tags in main_server_script.js")
-        for v in data.itemtags:
-            insert_after(server_lines, itemtag_marker, v)
-            progress.remove_task(itemtask)
-            blocktask = progress.add_task(description="Defining block tags in main_server_script.js")
-        for v in data.blocktags:
-            insert_after(server_lines, blocktag_marker, v)
-            progress.remove_task(blocktask)
-            fluidtask = progress.add_task(description="Defining fluid tags in main_server_script.js")
-        for v in data.fluidtags:
-            insert_after(server_lines, fluidtag_marker, v)
-            progress.remove_task(fluidtask)
-        print("Finished defining classes")
-        progress.add_task(description="Writing to main_server_script.js")
-        with open(main_server_script, "w", encoding="utf-8") as f:
-            for item in server_lines:
-                f.write(f"{item}\n")
+    
+    spinner.update("Defining recipe classes")
+    for v in data.recipes:
+        insert_after(server_lines, recipe_marker, v)
+        console.log(f"Finished Defining {v}", style="bold green")
+    console.log("Finished Defining recipe classes", style="bold green")
+    spinner.update("Defining item tag classes")
+    for v in data.itemtags:
+        insert_after(server_lines, itemtag_marker, v)
+        console.log(f"Finished Defining {v}", style="bold green")
+    console.log("Finished Defining item tag classes", style="bold green")
+    spinner.update("Defining block tag classes")
+    for v in data.blocktags:
+        insert_after(server_lines, blocktag_marker, v)
+        console.log(f"Finished Defining {v}", style="bold green")
+    console.log("Finished Defining block tag classes", style="bold green")
+    spinner.update("Defining fluid tag classes")
+    for v in data.fluidtags:
+        insert_after(server_lines, fluidtag_marker, v)
+        console.log(f"Finished Defining {v}", style="bold green")
+    console.log("Finished Defining fluid tag classes", style="bold green")
+    spinner.update("Writing to main_server_script.js")
+    with open(main_server_script, "w", encoding="utf-8") as f:
+        for item in server_lines:
+            f.write(f"{item}\n")
+    console.log("Finished writing to main_server_script.js", style="bold green")
+    console.log(f"Successfully installed {data.name}", style="bold green")
         
 def list_packages():
     packagefolder = Path.cwd() / "kubejs" / "server_scripts" / "external_packages"
