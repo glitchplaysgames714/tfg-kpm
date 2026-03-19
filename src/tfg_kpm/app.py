93e06cecf38dccd84ba3156ffa38e9f168f026c1
proga <fbgamer8374@gmail.com>
2026-03-19 13:59:27 -0400

    v2.0.0 Release

--- src/tfg_kpm/app.py
+++ src/tfg_kpm/app.py
@@ -1,25 +1,113 @@
+import time
+from pathlib import Path
+
 import typer
+from rich.console import Console
+from rich.table import Table
+
+from .commands.manager import install_package, list_packages, uninstall_package, update_packages
 from .core.errors import Errors
-from .commands.manager import install_package, list_packages, uninstall_package
+from .core.utils import error
 
 app = typer.Typer()
 
 @app.command()
 def install(repository: str, branch: str = "main"):
     Errors(["invalid_server"]).check()
-    
-    install_package(repository, branch)
+
+    console = Console()
+    start = time.perf_counter()
+
+    package = install_package(repository, branch)
+
+    end = time.perf_counter()
+    elapsed = end - start
+
+    if elapsed < 1:
+        console.log(f"""Installed {package} in [bright_cyan]{elapsed * 1000:.0f}[/bright_cyan]
+                    milliseconds""", style="bold green")
+    elif elapsed < 60:
+        console.log(f"Installed {package} in [bright_cyan]{elapsed:.2f}[/bright_cyan] seconds", style="bold green")
+    else:
+        minutes = elapsed / 60
+        console.log(f"Installed {package} in [bright_cyan]{minutes:.2f}[/bright_cyan] minutes", style="bold green")
 
 @app.command()
 def list():
     Errors(["invalid_server"]).check()
-    
-    
-    for package in list_packages():
-        print(package)
+
+    packages = list_packages()
+
+    console = Console()
+    table = Table(title="Installed packages")
+    table.add_column("Name")
+    table.add_column("Repo")
+    table.add_column("Branch")
+
+    for package in packages:
+        gitrepofile = Path.cwd() / "kubejs" / "server_scripts" / "external_packages" / package / "gitrepo"
+        if gitrepofile.is_file():
+            repo = gitrepofile.read_text(encoding="utf-8")
+            repo, branch = repo.split("@")
+            table.add_row(package, repo, branch)
+        else:
+            table.add_row(package, "Unknown", "Unknown")
+
+    console.print(table)
 
 @app.command()
 def uninstall(package: str):
     Errors(["invalid_server"]).check()
-    
-    uninstall_package(package)
+
+
+    installedpackages = list_packages()
+    if package not in installedpackages:
+        error(f"{package} is [red]not[/red] installed!")
+    console = Console()
+    packagedir = Path.cwd() / "kubejs" / "server_scripts" / "external_packages" / package
+
+    console.log(f"Found existing installation: {package}", style="bold green")
+    console.log(f"Uninstalling {package}:")
+    console.log("  Would remove:")
+    console.log(f"    [bright_cyan]{packagedir.as_posix()}[/bright_cyan]")
+    typer.confirm("Proceed?", abort=True)
+    start = time.perf_counter()
+
+    uninstall_package(package, packagedir)
+
+    end = time.perf_counter()
+    elapsed = end - start
+
+    if elapsed < 1:
+        console.log(f"""Uninstalled {package} in [bright_cyan]{elapsed * 1000:.0f}[/bright_cyan]
+                    milliseconds""", style="bold green")
+    elif elapsed < 60:
+        console.log(f"Uninstalled {package} in [bright_cyan]{elapsed:.2f}[/bright_cyan] seconds", style="bold green")
+    else:
+        minutes = elapsed / 60
+        console.log(f"Uninstalled {package} in [bright_cyan]{minutes:.2f}[/bright_cyan] minutes", style="bold green")
+
+@app.command()
+def update():
+
+    Errors(["invalid_server"]).check
+
+    console = Console()
+    start = time.perf_counter()
+
+    typer.confirm("Update all packages?", abort=True)
+
+    update_packages()
+
+    end = time.perf_counter()
+    elapsed = end - start
+
+    if elapsed < 1:
+        console.log(f"""Updated all packages in [bright_cyan]{elapsed * 1000:.0f}[/bright_cyan]
+                    milliseconds""", style="bold green")
+    elif elapsed < 60:
+        console.log(f"Updated all packages in [bright_cyan]{elapsed:.2f}[/bright_cyan] seconds", style="bold green")
+    else:
+        minutes = elapsed / 60
+        console.log(f"Updated all packages in [bright_cyan]{minutes:.2f}[/bright_cyan] minutes", style="bold green")
+
--- src/tfg_kpm/core/package.py
+++ src/tfg_kpm/core/package.py
@@ -1,6 +1,8 @@
-from .utils import error, fetch_toml, format_strings
 from dataclasses import dataclass
 from typing import Self
+
+from .utils import error, fetch_toml, format_strings
+
 
 @dataclass
 class Package:
@@ -9,26 +11,23 @@ class Package:
     itemtags: list[str]
     blocktags: list[str]
     fluidtags: list[str]
-    
-    
+
+
     @classmethod
     def from_git(cls, package:str, branch:str) -> Self:
         if package.count("/") != 1:
             error("Invalid package format, expected [red]author/repo[/red]")
-        
+
         data = fetch_toml(package, branch)
-        
+
         if not data or "tfg-kpm" not in data or "main" not in data["tfg-kpm"] or "package" not in data["tfg-kpm"]:
             error("Invalid [red]registry.toml[/red]")
-        
+
         main_data = data["tfg-kpm"]["main"]
         package_data = data["tfg-kpm"]["package"]
         if "name" not in main_data:
             error("Missing [red]name[/red] field in [red]registry.toml[/red]")
-        # removed this to allow for event only packages
-        # if "recipes" not in package_data and "itemtags" not in package_data and "blocktags" not in package_data and "fluidtags" not in package_data:
-        #     error("Empty [red]package[/red] field in [red]registry.toml[/red] this package does nothing!")
-        
+
         fields = {
             "name": main_data["name"],
             "recipes": format_strings(package_data.get("recipes", [])),
@@ -36,8 +35,7 @@ class Package:
             "blocktags": format_strings(package_data.get("blocktags", [])),
             "fluidtags": format_strings(package_data.get("fluidtags", []))
         }
-            
+
         return cls(**fields)
-        
-        
-        
+
+
